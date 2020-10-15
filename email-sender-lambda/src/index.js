348bd29158c5b8c546700c550576e6b0f134a9d3
Joao Paulo Santarem <jpstrm@gmail.com>
2020-10-15 09:42:01 -0300

    Fix indexjs

--- email-sender-lambda/src/index.js
+++ email-sender-lambda/src/index.js
@@ -5,14 +5,26 @@ module.exports.handler = async (event, context) => {
   console.log('context', context)
   logger.info('Inicializando função de envio de emails')
   let total = 0
-  event.Records.forEach(entry => {
-    emailService.send(entry.body)
-    total++
-  })
-  const successMsg = 'Emails enviados com sucesso, total: ' + total
-  logger.info(successMsg)
-  return {
-    statusCode: 200,
-    message: successMsg
+  if (event && event.Records && event.Records.length) {
+    event.Records.forEach(entry => {
+      emailService.send(entry.body)
+      total++
+    })
+    const successMsg = 'Emails enviados com sucesso, total: ' + total
+    logger.info(successMsg)
+    return {
+      statusCode: 200,
+      body: {
+        message: successMsg
+      }
+    }
+  } else {
+    return {
+      statusCode: 500,
+      body: {
+        message: 'Não foi possível processar o envio dos emails'
+      }
+    }
   }
+
 }
